4e1f935109156fea1fb3e54deaa31b76f688a5e8
edulcni <cpgxny5272@privaterelay.appleid.com>
2026-09-05 15:17:01 +0800

    Update AAA.cpp

--- AAA.cpp
+++ AAA.cpp
@@ -4,6 +4,11 @@
 #include <filesystem>
 using namespace std;// /Users/macmini/Desktop/table/xcode-c++/A/A/main.cpp
 string bbh="00011";
+void mian();
 int main(){
     cout << bbh;
+    mian();
+}
+void mian(){
+    cout << "hello wored";
 }
